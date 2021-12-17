25ba5b2d57e3696b570d913effecc3ca4a3488c7
Hassan Syyid <hassansyyid@gmail.com>
2021-12-17 14:34:21 -0500

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='target-shopify',
-    version='0.0.3',
+    version='0.0.4',
     description='hotglue target for exporting data to Shopify API',
     author='hotglue',
     url='https://hotglue.xyz',
